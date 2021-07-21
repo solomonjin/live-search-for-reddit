4b3187582a0b26b4fd7db597e8ac7a4dab103cf0
Solomon Jin <solomonjin135@yahoo.com>
2021-07-21 18:19:28 +0000

    Created an authentication middleware that will check tos ee if requests contain a signed cookie

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const pg = require('pg');
 const snoowrap = require('snoowrap');
 const snoostorm = require('snoostorm');
@@ -20,6 +21,23 @@ const app = express();
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
+
+app.use(express.json());
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.get('/api/auth', (req, res, next) => {
+  let payload;
+  if (!req.signedCookies.userToken) {
+    payload = {
+      user: null,
+      userID: null
+    };
+  } else {
+    payload = jwt.verify(req.signedCookies.userToken, process.env.TOKEN_SECRET);
+  }
+  res.json(payload);
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
