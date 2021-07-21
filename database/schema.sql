10fae0c4a8440d7dd82e655f5c32be282e380d28
Solomon Jin <solomonjin135@yahoo.com>
2021-07-21 01:00:13 +0000

    Updated schema

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,30 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."users" (
+	"userID" serial NOT NULL,
+	"username" TEXT NOT NULL UNIQUE,
+	"refreshToken" TEXT NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userID")
+) WITH (
+  OIDS=FALSE
+);
+
+`
+
+CREATE TABLE "public.subscriptions" (
+	"subscriptionID" serial NOT NULL,
+	"userID" integer NOT NULL,
+	"keywords" TEXT NOT NULL,
+	"subreddit" TEXT NOT NULL,
+	CONSTRAINT "subscriptions_pk" PRIMARY KEY ("subscriptionID")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_fk0" FOREIGN KEY ("userID") REFERENCES "users"("userID");
