5ada0589af7fbc648fada0bd8e080b469c1f7ade
Solomon Jin <solomonjin135@yahoo.com>
2021-07-21 20:50:36 +0000

    If isAuthorizing is true, nothing will be rendered

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './pages/home';
 
 export default function App(props) {
   const [user, setUser] = useState(null);
   const [isAuthorizing, setIsAuthorizing] = useState(true);
 
+  if (isAuthorizing) return;
   return <Home />;
 }
