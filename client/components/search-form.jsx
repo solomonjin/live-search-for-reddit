6cdb78e900ff3d0c127b3afd197d8e9aab97e838
Solomon Jin <solomonjin135@yahoo.com>
2021-07-24 01:04:31 +0000

    Search form will retain previous values when it slides off of screen and re-enters

--- client/components/search-form.jsx
+++ client/components/search-form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {
   IconButton, TextField, Grid, ClickAwayListener,
-  FormControl, Slide, makeStyles
+  FormControl, Slide, makeStyles, Button
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
@@ -23,11 +23,23 @@ const useStyles = makeStyles(theme => ({
       height: 140,
       padding: '10px'
     }
+  },
+  form: {
+    width: '100%'
+  },
+  button: {
+    background: 'linear-gradient(45deg, #A3D8F7 30%, #C5CFF3 90%)',
+    boxShadow: '0 3px 5px 2px rgba(197, 207, 243, .3)',
+    color: '#393E41'
   }
 }));
 
 export default function SearchForm(props) {
   const [isOpen, setOpen] = useState(false);
+  const [keywords, setKeywords] = useState('');
+  const [subs, setSubs] = useState('');
+  const [toggleInbox, setToggleInbox] = useState(false);
+
   const classes = useStyles();
 
   const handleOpen = () => {
@@ -43,6 +55,14 @@ export default function SearchForm(props) {
     console.log(event);
   };
 
+  const changeKeywords = event => {
+    setKeywords(event.target.value);
+  };
+
+  const changeSubs = event => {
+    setSubs(event.target.value);
+  };
+
   return (
     <ClickAwayListener onClickAway={handleClose}>
       <div>
@@ -50,15 +70,36 @@ export default function SearchForm(props) {
           <SearchIcon />
         </IconButton>
 
-        <Slide direction="down" in={isOpen} mountOnEnter unmountOnExit>
+        <Slide direction="down" in={isOpen} mountOnEnter>
           <div className={classes.root}>
-            <FormControl onSubmit={handleSubmit}>
-              <Grid container>
-                <Grid item xs={12} sm={6}>
-                  <TextField fullWidth variant="outlined" label="Keywords" />
+            <FormControl className={classes.form} onSubmit={handleSubmit}>
+              <Grid container spacing={3} alignItems="center" justifyContent="space-evenly">
+                <Grid item xs={12} sm={10} align="center">
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField fullWidth required
+                        variant="outlined" label="Keywords"
+                        placeholder="Separated by commas"
+                        onChange={changeKeywords} />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField fullWidth required
+                        variant="outlined" label="Subreddits"
+                        placeholder="Separated by commas"
+                        onChange={changeSubs} />
+                    </Grid>
+                    <Grid item xs={12}>
+
+                    </Grid>
+                  </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField fullWidth variant="outlined" label="Subreddits" />
+
+                <Grid item xs={12} sm={1} align="end">
+                  <Grid container justifyContent="center">
+                    <Button className={classes.button}>
+                      Search
+                    </Button>
+                  </Grid>
                 </Grid>
               </Grid>
             </FormControl>
