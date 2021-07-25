e8d8270ea99c3e1149c1e473b84104e17eee6507
Solomon Jin <solomonjin135@yahoo.com>
2021-07-25 12:57:32 -0700

    Fixed an issue where the dates were not being formatted properly

--- client/lib/format-date.js
+++ client/lib/format-date.js
@@ -1,15 +1,11 @@
 export default function formatDate(time) {
   const date = new Date(time * 1000);
   const month = date.toLocaleString('default', { month: 'short' });
-  const day = date.getDay();
+  const day = date.getDate();
   let hour = date.getHours();
   const min = date.getMinutes();
-  let twelveHour;
-  if (hour > 12) {
-    hour -= 12;
-    twelveHour = 'PM';
-  } else {
-    twelveHour = 'AM';
-  }
+  const twelveHour = hour >= 12 ? 'PM' : 'AM';
+  hour = hour % 12;
+  hour = hour || 12;
   return `${month} ${day} ${hour}:${min.toString().length < 2 ? '0' + min : min} ${twelveHour}`;
 }
