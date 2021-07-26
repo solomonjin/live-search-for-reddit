0591d8228a0c26b4ca9173adf2d19d3cc35f388c
Solomon Jin <solomonjin135@yahoo.com>
2021-07-26 17:21:45 +0000

    added a condition to the SubmissionBody component to verify that a Submission contains body text. Otherwise, it will return a React fragment.

--- client/components/submission-body.jsx
+++ client/components/submission-body.jsx
@@ -3,6 +3,7 @@ import DOMPurify from 'dompurify';
 import { Typography } from '@material-ui/core';
 
 export default function SubmissionBody(props) {
+  if (!props.text) return (<></>);
   const clean = DOMPurify.sanitize(props.text);
   return <Typography variant="body2" dangerouslySetInnerHTML={{ __html: clean }} />;
 }
