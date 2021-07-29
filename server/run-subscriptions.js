29c5003cccdeb373deaca23be06db2620ae554ab
Solomon Jin <solomonjin135@yahoo.com>
2021-07-29 00:22:10 +0000

    Manually closed any idle db connections that were causing end lag

--- server/run-subscriptions.js
+++ server/run-subscriptions.js
@@ -23,58 +23,63 @@ const sql = `
 
 db.query(sql)
   .then(result => {
-    result.rows.forEach(sub => {
-      const keywords = parseKeywords(sub.keywords).join(' OR ');
-      const subreddits = parseSubs(sub.subreddits);
-      const subTime = Math.floor(new Date(sub.createdAt).getTime() / 1000);
-
-      const searchParams = {
-        query: keywords,
-        subreddit: subreddits,
-        sort: 'new',
-        time: 'hour'
-      };
-
-      return r
-        .search(searchParams)
-        .then(listings => {
-          const submissions = [];
-          listings.forEach(post => {
-            if (subTime > post.created_utc) return;
-            submissions.push({
-              title: post.title,
-              url: post.url
-            });
-          });
-          if (submissions.length === 0) {
-            return updateTime(sub.subscriptionId);
-          }
-
-          const text = submissions.reduce((msg, line) => {
-            return msg + `[${line.title}](${line.url})` + '\n\n';
-          }, '');
-          const message = {
-            to: sub.username,
-            subject: `${submissions.length} results found`,
-            text
-          };
-          return r
-            .composeMessage(message)
-            .then(result => {
-              return updateTime(sub.subscriptionId);
-            });
-        });
-    });
+    const subscriptionArr = result.rows.map(sub => runSubscription(sub));
+    return Promise
+      .allSettled(subscriptionArr)
+      .then(() => {
+        db.end();
+      });
   })
   .catch(err => console.error(err));
 
 const updateTime = subId => {
   const sql = `
-                  update "subscriptions"
-                     set "createdAt" = now()
-                   where "subscriptionId" = $1
-              `;
+    update "subscriptions"
+        set "createdAt" = now()
+      where "subscriptionId" = $1
+    `;
   const params = [subId];
 
   return db.query(sql, params);
+};
+
+const runSubscription = sub => {
+  const keywords = parseKeywords(sub.keywords).join(' OR ');
+  const subreddits = parseSubs(sub.subreddits);
+  const subTime = Math.floor(new Date(sub.createdAt).getTime() / 1000);
+
+  const searchParams = {
+    query: keywords,
+    subreddit: subreddits,
+    sort: 'new',
+    time: 'hour'
+  };
+  return r
+    .search(searchParams)
+    .then(listings => {
+      const submissions = [];
+      listings.forEach(post => {
+        if (subTime > post.created_utc) return;
+        submissions.push({
+          title: post.title,
+          url: post.url
+        });
+      });
+      if (submissions.length === 0) {
+        return updateTime(sub.subscriptionId);
+      }
+      const text = submissions.reduce((msg, line) => {
+        return msg + `[${line.title}](${line.url})` + '\n\n';
+      }, '');
+      const message = {
+        to: sub.username,
+        subject: `${submissions.length} results found`,
+        text
+      };
+      return r
+        .composeMessage(message)
+        .then(result => {
+          return updateTime(sub.subscriptionId);
+        });
+    });
 };
